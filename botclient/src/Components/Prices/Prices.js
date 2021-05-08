09db84c8b8b795282bdc8615e8bce4df86f296b4
Sakon Thephamongkhol <kbdirifilation@gmail.com>
2021-05-08 22:39:41 +0700

    trying

--- botclient/src/Components/Prices/Prices.js
+++ botclient/src/Components/Prices/Prices.js
@@ -1,18 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Coin from "../../Coins/CoinClass";
 import CoinList from "../../Coins/Coins";
 import PriceCard from "./PriceCard";
 import Mode from "../../Values/CoinValueMode";
 import $ from "jquery";
 
-class Prices extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Prices: [],
-    };
-  }
-  addNewCoin() {
+const Prices = (props) => {
+  const [CoinListState, setCoinListState] = useState(CoinList);
+
+  function addNewCoin() {
     const symbol = $("#symbol").val();
     const pair = $("#pair").val();
     const exchange = $("#exchange").val();
@@ -20,41 +16,60 @@ class Prices extends React.Component {
     console.log(CoinList);
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <div style={{ width: "100vw", overflowX: "hidden" }}>
-          <div
-            className="row"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <h2>Coins in watchlist</h2>
-          </div>
-          <div className="row ml-2 mr-2 mt-4">
-            {CoinList.map((coin) => (
-              <div
-                className="col-3 mb-3"
-                style={{
-                  width: "100vw",
-                  display: "flex",
-                  justifyContent: "center",
-                }}
+  // for (var i = 0; i < CoinList.length; i++) {
+  //   const coin = CoinList[i];
+  //   useEffect(() => {
+  //     const index = CoinListState.indexOf(coin);
+  //     let newCoinList = CoinList;
+  //     newCoinList[index] = coin;
+  //     setCoinListState(newCoinList);
+  //   }, [
+  //     coin.symbol,
+  //     coin.exchange,
+  //     coin.amount,
+  //     coin.percentage,
+  //     coin.basePrice,
+  //     coin.priceNow,
+  //     coin.side,
+  //     coin.abbr,
+  //     coin.up,
+  //   ]);
+  // }
+
+  return (
+    <React.Fragment>
+      <div style={{ width: "100vw", overflowX: "hidden" }}>
+        <div
+          className="row"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <h2>Coins in watchlist</h2>
+        </div>
+        <div className="row ml-2 mr-2 mt-4">
+          {CoinList.map((coin) => (
+            <div
+              className="col-3 mb-3"
+              style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center",
+              }}
+              key={coin.symbol}
+            >
+              <PriceCard
+                coin={coin}
+                showbg={true}
+                showname={true}
                 key={coin.symbol}
-              >
-                <PriceCard
-                  coin={coin}
-                  showbg={true}
-                  showname={true}
-                  key={coin.symbol}
-                  mode={Mode.NORMAL}
-                />
-              </div>
-            ))}
-          </div>
-          {/* <div className="row ml-3">
+                mode={Mode.NORMAL}
+              />
+            </div>
+          ))}
+        </div>
+        {/* <div className="row ml-3">
             <h3 style={{ width: "100vw" }}>Add a new coin into watchlist</h3>
             <div className="mr-3">
               <label htmlFor="symbol" className="mr-3">
@@ -91,10 +106,9 @@ class Prices extends React.Component {
               Create
             </button>
           </div> */}
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Prices;
